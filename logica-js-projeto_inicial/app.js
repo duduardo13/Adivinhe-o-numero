0e29a3e6021b87f1a4f0face7b11d56a71d37822
Eduardo Barbosa Rodrigues <eduardo.rodrigues@modalgr.io>
2025-02-25 11:19:29 -0300

    Introduzindo condição e repetição

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,32 @@
+alert('Boas vindas ao jogos do número secreto');
+
+let numero_secreto = 29;
+console.log(numero_secreto);
+let chute;
+let tentativas = 1;
+
+//Enquanto o chute não for certo
+
+while(chute != numero_secreto){
+    chute= prompt('Digite o número entre 1 e 30');
+
+    if (numero_secreto == chute){
+        alert( `Boa ${numero_secreto}`);
+     }else{
+         alert('Burro');
+             
+            if(numero_secreto > chute){
+                 alert(`O número secreto é maior que o ${chute}`);
+             }else{
+                 alert(`O número secreto é menor que o ${chute}`)
+             }
+            
+        tentativas++; 
+        
+     }
+     
+    
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numero_secreto} com ${tentativas} ${palavraTentativa}`);
